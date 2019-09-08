1b21c16a6212014d037a511b9b89decf92ac6abf
Nishant Parmar <55044190+nsp8@users.noreply.github.com>
2019-09-08 13:46:35 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,4 @@
+# Constants for airquality_preprocessor.py:
+api_key = '...' #generate your own key
+resource_id = '...' #resource id (API-specific) also found at the website
+output_base_path = 'your-local-repo'
